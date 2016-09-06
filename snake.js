1aea80b784c0fa4a5df0a2a123e9e0e14ec36365
A.J. Cabanatuan <allanjay808@ksu.edu>
2016-09-05 21:18:45 -0500

    Out of bounds kinda works, need to modify direction ability

--- snake.js
+++ snake.js
@@ -14,6 +14,7 @@ var y = 0;
 
 // Initialize Snake
 var snake = [];
+var apple;
 
 var input = {
   up: false,
@@ -22,22 +23,22 @@ var input = {
   right: false
 }
 
+/**
+  * @function init
+  * Initializes or restarts the game
+  */
 function init() {
-  snake.push(new Square(150, 30));
-  snake.push(new Square(140, 30));
-  snake.push(new Square(130, 30));
-  snake.push(new Square(120, 30));
-  snake.push(new Square(110, 30));
-  snake.push(new Square(100, 30));
-  snake.push(new Square(90, 30));
-  snake.push(new Square(80, 30));
-  snake.push(new Square(70, 30));
-  snake.push(new Square(60, 30));
-  snake.push(new Square(50, 30));
-  snake.push(new Square(40, 30));
-  snake.push(new Square(30, 30));
+  frontCtx.drawImage(backBuffer, 0, 0);
+  spawnSnake();
 
-  input.right = true;
+  resetDirection();
+
+  var appleX = Math.ceil(Math.random() * (75 - 1 + 1) + 4) * 10;
+  var appleY = Math.ceil(Math.random() * (47 - 4 + 1) + 4) * 10;
+
+  spawnApple(appleX, appleY);
+  console.log("apple pos x: ", apple.x);
+  console.log("apple pos y: ", apple.y);
 
   update();
   render();
@@ -80,38 +81,39 @@ loop();
 function update(elapsedTime) {
 
   // TODO: Spawn an apple periodically
+  drawApple(apple.x, apple.y);
+
   // TODO: Grow the snake periodically
+  // growSnake();
 
   // TODO: Move the snake
-  // Move head
   var moveX = snake[0].x;
   var moveY = snake[0].y;
+  console.log("Snake head pos X: ", moveX);
+  console.log("Snake head pos Y: ", moveY);
 
   if (input.up) {
-    snake[0].y -= 10;
+    moveY -= 10;
   }
-  else if (input.down) {
-    snake[0].y += 10;
+  if (input.down) {
+    moveY += 10;
   }
-  else if (input.left) {
-    snake[0].x -= 10;
+  if (input.left) {
+    moveX -= 10;
   }
-  else if (input.right) {
-    snake[0].x += 10;
+  if (input.right) {
+    moveX += 10;
   }
 
-  var tempY = 0;
-  var tempX = 0;
-  for (var i = 1; i < snake.length; i++) {
-    tempY = snake[i].y;
-    tempX = snake[i].x;
-    snake[i].y = moveY;
-    snake[i].x = moveX;
-    moveY = tempY;
-    moveX = tempX;
-  }
+  var end = snake.pop();
+  end.x = moveX;
+  end.y = moveY;
+  snake.unshift(end);
 
   // TODO: Determine if the snake has moved out-of-bounds (offscreen)
+  // FIXME: Border of screen is off
+  checkOutOfBounds();
+
   // TODO: Determine if the snake has eaten an apple
   // TODO: Determine if the snake has eaten its tail
   // TODO: [Extra Credit] Determine if the snake has run into an obstacle
@@ -120,7 +122,7 @@ function update(elapsedTime) {
 /**
   * @function render
   * Renders the current game state into a back buffer.
-  * @param {elapsedTime} A DOMHighResTimeStamp indicting
+  * @param {elapsedTime} DOMHighResTimeStamp indicting
   * the number of milliseconds passed since the last frame.
   */
 function render(elapsedTime) {
@@ -129,29 +131,82 @@ function render(elapsedTime) {
   backCtx.fillStyle = "black";
   backCtx.fillRect(0, 0, backBuffer.width, backBuffer.height);
 
-  // backCtx.fillStyle = "green";
-  // backCtx.fillRect(30, 30, 50, 50);
-
   for (var i = 0; i < snake.length; i++) {
     drawSquare(snake[i].x, snake[i].y);
-    // drawCircle(snake[i].x, snake[i].y);
   }
 
+  drawApple(apple.x, apple.y);
 }
 
 /**
   * @function drawSqaure
+  * Draws part of the snake given the coordinates
+  * @param {x} X coordinate at which to draw
+  * @param {y} Y coordinate at which to draw
   */
 function drawSquare(x, y) {
   backCtx.fillStyle = "green";
   backCtx.fillRect(x, y, 10, 10);
-  backCtx.strokeStyle = "white";
+  backCtx.strokeStyle = "black";
   backCtx.strokeRect(x, y, 10, 10);
 }
 
+/**
+  * @function Square
+  * A single square object that is used to add to the snake
+  * @param {x} X coordinate of the square
+  * @param {y} Y coordinate of the square
+  */
 function Square(x, y) {
   this.x = x;
   this.y = y;
+}
+
+/**
+  * @function spawnApple
+  * This will set the coordinates of the apple
+  * @param {appleX} X random coordinate of the apple
+  * @param {appleY} Y random coordinate of the apple
+  */
+function spawnApple(appleX, appleY) {
+  apple = {
+    x: appleX,
+    y: appleY,
+  };
+}
+
+/**
+  * @function drawApple
+  * Draws the apple given the coordinates
+  * @param {x} X coordinate at which to draw
+  * @param {y} Y coordinate at which to draw
+  */
+function drawApple(x, y) {
+  backCtx.fillStyle = "red";
+  backCtx.fillRect(x, y, 10, 10);
+  backCtx.strokeStyle = "black";
+  backCtx.strokeRect(x, y, 10, 10);
+}
+
+function checkOutOfBounds() {
+  if (snake[0].x < -1 || snake[0].y < -1 || snake[0].x > 761 || snake[0].y > 481) {
+    snake = [];
+    resetDirection();
+    init();
+  }
+}
+
+function spawnSnake() {
+  snake.push(new Square(120, 30));
+  snake.push(new Square(110, 30));
+  snake.push(new Square(100, 30));
+  snake.push(new Square(90, 30));
+  snake.push(new Square(80, 30));
+  snake.push(new Square(70, 30));
+  snake.push(new Square(60, 30));
+  snake.push(new Square(50, 30));
+  snake.push(new Square(40, 30));
+  snake.push(new Square(30, 30));
 }
 
 window.onkeydown = function(event) {
@@ -192,6 +247,13 @@ window.onkeydown = function(event) {
       break;
   }
   return false;
+}
+
+function resetDirection() {
+  input.up = false;
+  input.down = false;
+  input.left = false;
+  input.right = true;
 }
 
 /* Launch the game */
